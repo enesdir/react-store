b05dd436de0f5af39a876303226278f50faf6cf2
Enes Esen <enesen@gmx.com>
2023-10-28 14:28:53 +0300

    fix: production error about case sensitiviy

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import Container from '@mui/material/Container'
+
+import { HeroCarousel } from '@/features/heroSection/components/HeroCarousel/HeroCarousel'
+import { ProductsSection } from '@/features/products/components/ProductsSection'
+
+const Home = () => {
+	return (
+		<>
+			<HeroCarousel />
+			<Container sx={{ paddingY: 10 }}>
+				<ProductsSection />
+			</Container>
+			{/* <ProductCardGrid products={testData} /> */}
+		</>
+	)
+}
+
+export default Home
---
+++ src/pages/TemplateTest/index.tsx
@@ -0,0 +1,7 @@
+import TemplateTester from './components/TemplateTester'
+
+const TemplateTest = () => {
+	return <TemplateTester />
+}
+
+export default TemplateTest
