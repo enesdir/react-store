89ee2db360c4813a478233279def37de1f353b04
Enes Esen <enesen@gmx.com>
2023-10-28 14:13:16 +0300

    fix: production build error

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,18 +3,18 @@ import { Pages, Routes } from './types'
 
 export const routes: Routes = {
 	[Pages.Home]: {
-		component: asyncComponentLoader(() => import('@/pages/Home')),
+		component: asyncComponentLoader(() => import('../pages/Home')),
 		path: '/',
 		title: 'Home',
 	},
 	[Pages.TemplateTest]: {
-		component: asyncComponentLoader(() => import('@/pages/TemplateTest')),
+		component: asyncComponentLoader(() => import('../pages/TemplateTest')),
 		path: '/templatetest',
 		title: 'Template Test',
 	},
 
 	[Pages.NotFound]: {
-		component: asyncComponentLoader(() => import('@/pages/NotFound')),
+		component: asyncComponentLoader(() => import('../pages/NotFound')),
 		path: '*',
 	},
 }
