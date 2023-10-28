b520e53b6e5137607bce779cce8074d596c43eab
Enes Esen <enesen@gmx.com>
2023-10-28 14:09:52 +0300

    fix: production build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 /// <reference types="vite/client" />
+import * as path from 'path'
+
 import react from '@vitejs/plugin-react-swc'
 import million from 'million/compiler'
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [million.vite({ auto: true }), react(), tsConfigPaths()],
+	plugins: [million.vite({ auto: true }), react()],
 	server: {
 		open: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
 	},
 })
