e537d6a3d866dbf82a7e1b63f6b9c4de872570d8
Enes Esen <enesen@gmx.com>
2023-10-30 08:56:56 +0300

    fix: ui improvements for notfound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,19 +1,25 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { Link } from 'react-router-dom'
+
+import { CenteredFlexBox } from '@/components/styled'
+
 const NotFound = () => {
 	return (
-		<div className='grid md:flex md:min-h-[60vh] md:items-center gap-4'>
-			<div className='text-center'>
-				<img width={400} src='/assets/404 Error-rafiki.svg' alt='404' />
-			</div>
+		<CenteredFlexBox
+			className='md:flex md:items-center'
+			minHeight={{ xs: '40vh', md: '60vh', lg: '70vh', flexFlow: 'column' }}
+			gap={2}
+		>
+			<Box sx={{ justifyContent: 'center' }}>
+				<img width={500} src='/assets/404 Error-rafiki.svg' alt='404' />
+			</Box>
 
-			<div className='text-center md:text-start'>
-				<h1 className='text-3xl'>Page not Found</h1>
-
-				<div className='grid gap-2'>
-					<p>It&apos;s Okay!</p>
-					<div></div>
-				</div>
-			</div>
-		</div>
+			<Box sx={{ justifyContent: 'center', alignItems: 'flex-start', paddingBottom: 10 }}>
+				<Typography variant='h1'>Page not Found</Typography>
+				<Link to={'/'}>Go Home</Link>
+			</Box>
+		</CenteredFlexBox>
 	)
 }
 
